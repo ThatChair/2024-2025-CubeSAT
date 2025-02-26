5c070a219aba33dd4dc3893fdc865b3528d2c0c7
Chair <teammoorefamily@gmail.com>
2025-02-26 15:32:37 -0800

    kinda works now

--- FinalProject/utils/ImageAnalyzer.py
+++ FinalProject/utils/ImageAnalyzer.py
@@ -1,16 +1,25 @@
-from PIL import Image, ImageFilter, ImageChops
+from PIL import Image, ImageFilter, ImageChops, ImageEnhance
 
-def take_diff(before_image:str, after_image:str):
-    bfr = Image.open(before_image)
-    aftr = Image.open(after_image)
-    diff = ImageChops.difference(bfr,aftr)
-    return diff
+def take_diff(before_image: Image, after_image: Image):
+    return ImageChops.difference(before_image,after_image)
 
-def find_nonpassable(after_image:str):
-    image = Image.open(after_image)
-    image = Image.convert("L")
-    edges = image.filter(imageFilter.FIND_EDGES)
-    nonpassable = edges.filter(imageFilter.BLUR)
+def find_edges(raw_image: Image) -> Image:
+    image = raw_image.convert("L")
+    return image.filter(ImageFilter.FIND_EDGES).filter(ImageFilter.EDGE_ENHANCE_MORE).convert("RGBA")
 
+def find_nonpassable(image: Image) -> Image:
+    new_data = []
+    for datum in image.getdata():
+        new_data.append((255, 0, 0, datum[0] * 2))
 
-    
+    image.putdata(new_data)
+    return image
+
+before = Image.open("C:\\Users\\teamm\\PycharmProjects\\2024-2025-CubeSAT\\FinalProject\\test_files\\Milton_Before.png")
+after = Image.open("C:\\Users\\teamm\\PycharmProjects\\2024-2025-CubeSAT\\FinalProject\\test_files\\Milton_After.png")
+edge_before = find_nonpassable(find_edges(before))
+edge_after = find_nonpassable(find_edges(after).filter(ImageFilter.BLUR).filter(ImageFilter.BLUR).filter(ImageFilter.BLUR))
+diff = take_diff(before, after)
+
+Image.alpha_composite(after.convert("RGBA"), edge_after).show()
+
