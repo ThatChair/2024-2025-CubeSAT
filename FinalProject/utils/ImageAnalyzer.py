4ee6a6cbefea917694ac4e5d052e73cb2c849fc7
Chair <teammoorefamily@gmail.com>
2025-02-26 23:57:17 -0800

    enhanced a little more

--- FinalProject/utils/ImageAnalyzer.py
+++ FinalProject/utils/ImageAnalyzer.py
@@ -15,25 +15,27 @@ def process_edges(image: Image) -> Image:
     data = image.getdata()
     average = data[0][0]
     num = 1
+    max = average
     for i in range(len(data) - 2):
-        if data[i + 1][0] > 5:
+        if data[i + 1][0] > 0:
             average = calculate_average(average, data[i + 1][0], num)
             num += 1
+            if data[i + 1][0] > max:
+                max = data[i + 1][0]
+
+    multiplier = 255 / max
 
     for datum in image.getdata():
         if datum[0] >= average:
-            new_data.append((255, 0, 0, 255))
+            new_data.append((255, 0, 0, int(datum[0] * multiplier)))
         else:
             new_data.append((0, 0, 0, 0))
 
     image.putdata(new_data)
     return image
 
-before = Image.open("C:\\Users\\teamm\\PycharmProjects\\2024-2025-CubeSAT\\FinalProject\\test_files\\Milton_Before.png")
 after = Image.open("C:\\Users\\teamm\\PycharmProjects\\2024-2025-CubeSAT\\FinalProject\\test_files\\Milton_After.png")
-edge_before = process_edges(find_edges(before))
-edge_after = process_edges(find_edges(after).filter(ImageFilter.SMOOTH))
-diff = take_diff(before, after)
+edge_after = process_edges(find_edges(after.filter(ImageFilter.SHARPEN)).filter(ImageFilter.SMOOTH))
 
 out = Image.alpha_composite(after.convert("RGBA"), edge_after)
 out.putalpha(255)
